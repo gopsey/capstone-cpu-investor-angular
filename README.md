a5ee45005ce4c0d61aa2fa17244ab595e77bbc03
Gopalakrishnan <gopalkris96@gmail.com>
2024-06-11 22:33:03 +0530

    Lazy loading implemented and reduced build file size

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # capstone-cpu-investor-angular
 CPU Investor Center Angular Application Capstone project
+
+# Implementations
+Angular Workspaces Monorepo concept with Applications and Libaries
+Lazy loading based routing components (Reduced build size from 1.19 MB to 518 kB)
+Capacitor for Android using Angular
+Angular Material components
+
+# Commands
+ng g application my-app
+ng g library my-lib
+npm install @capacitor/angular --force
+ng build CapstoneCpuInvestorAngular --configuration production --aot
+ng serve CapstoneCpuInvestorAngular
--- projects/cpu-investor-computershare/src/app/app.routes.ts
+++ projects/cpu-investor-computershare/src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router';
-import { PortfolioComponent } from './features/portfolio/portfolio.component';
-import { ActivityComponent } from './features/activity/activity.component';
-import { AccountComponent } from './features/account/account.component';
 
 export const routes: Routes = [
-   { path: '', component: PortfolioComponent },
-   { path: 'portfolio', component: PortfolioComponent },
-   { path: 'activity', component: ActivityComponent },
-   { path: 'account', component: AccountComponent },
+   {
+      path: '',
+      loadComponent: () => import('./features/portfolio/portfolio.component').then((x) => x.PortfolioComponent),
+   },
+   {
+      path: 'portfolio',
+      loadComponent: () => import('./features/portfolio/portfolio.component').then((x) => x.PortfolioComponent),
+   },
+   {
+      path: 'activity',
+      loadComponent: () => import('./features/activity/activity.component').then((x) => x.ActivityComponent),
+   },
+   {
+      path: 'account',
+      loadComponent: () => import('./features/account/account.component').then((x) => x.AccountComponent),
+   },
 ];
